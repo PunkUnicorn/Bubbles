f113abcb3befb82d55530eebc322e9ad7f057deb
PunkUnicorn <matthewcocks@hotmail.com>
2014-04-28 11:03:45 +0100

    GCC compatibility change

--- Bubbles/BubbleVariables.h
+++ Bubbles/BubbleVariables.h
@@ -6,17 +6,24 @@
 #include <map>
 
 
+#if _MSC_VER
+#   define COMPILE_MESSAGE(exp) #pragma message(exp)
+#else//__GNUC__ - may need other defines for different compilers
+#   define COMPILE_MESSAGE(exp) #pragma message exp
+#endif
+
+
 // http://www.chris.com/ascii/index.php?art=animals/birds%20%28land%29
-#pragma message("        .---.        .-----------")
-#pragma message("       /     \\  __  /    ------   bubble build bird bows and greets you")
-#pragma message("      / /     \\(--)/    -----")
-#pragma message("     //////   ' \\/ `   ---")
-#pragma message("    //// / // :    : ---")
-#pragma message("   // /   /  /`    '--")
-#pragma message("  //          //..\\\\")
-#pragma message("=============UU====UU====")
-#pragma message("             '//||\\\\`")
-#pragma message("               ''``")
+COMPILE_MESSAGE("        .---.        .-----------")
+COMPILE_MESSAGE("       /     \\  __  /    ------   bubble build bird bows and greets you")
+COMPILE_MESSAGE("      / /     \\(--)/    -----")
+COMPILE_MESSAGE("     //////   ' \\/ `   ---")
+COMPILE_MESSAGE("    //// / // :    : ---")
+COMPILE_MESSAGE("   // /   /  /`    '--")
+COMPILE_MESSAGE("  //          //..\\\\")
+COMPILE_MESSAGE("=============UU====UU====")
+COMPILE_MESSAGE("             '//||\\\\`")
+COMPILE_MESSAGE("               ''``")
 
 static std::vector<cBubbleEngine::PTR> engines;
 static std::map<unsigned int /*engineGroupId*/, std::vector<unsigned int /*engineId*/>> engineGroups;
