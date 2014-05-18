6926fb18c9487adda3c93a05051edf7bf599eb49
PunkUnicorn <matthewcocks@hotmail.com>
2014-05-18 13:37:48 +0100

    Slight tidy

--- Bubbles/BubbleEngine.h
+++ Bubbles/BubbleEngine.h
@@ -13,64 +13,10 @@
 #include "BubbleTimer.h"
 #include "BubbleDimensionCracker.h"
 #include "BubbleFindCollisions.h"
+#include "BubbleCollisionReportThread.h"
 
 namespace Bubbles
 {
-
-class cBubbleCollisionReportThread 
-{
-private:
-   typedef struct tagREPORT_PAYLOAD
-   {
-      unsigned int groupID; 
-      unsigned int engineId; 
-      COLLISION_RESULT* list; 
-      unsigned int size;
-      CollisionReportFunc *reportFunc;
-      std::allocator<tagREPORT_PAYLOAD> *allocator;
-   } REPORT_PAYLOAD;
-
-   std::allocator<REPORT_PAYLOAD> mPayloadAllocator;
-   SDL_Thread *mThreadID;
-
-   static int thread_function(void *p)
-   {
-      if (p == NULL) return 0;
-      REPORT_PAYLOAD &payload = *(REPORT_PAYLOAD *)p;
-        
-      payload.reportFunc(payload.groupID, payload.engineId, payload.list, payload.size);
-      payload.allocator->deallocate(&payload, 1);
-
-      //                
-      //             ,\ ! /, 
-      //            -- POP --
-      //             '/ ! \' 
-      //
-
-      return 0;
-   };
-
-public:
-   cBubbleCollisionReportThread(void) : mPayloadAllocator(), mThreadID(NULL) { };
-   void Init(void) { mThreadID = SDL_CreateThread(thread_function, NULL); };
-   static void Start(cBubbleCollisionReportThread &me, CollisionReportFunc *func, unsigned int pgroupID, unsigned int pengineId, COLLISION_RESULT* plist, unsigned int psize)
-   {
-      REPORT_PAYLOAD *p = me.mPayloadAllocator.allocate(1);
-      REPORT_PAYLOAD &payload = *p;
-
-      payload.reportFunc = func;
-      payload.groupID = pgroupID;
-      payload.engineId = pengineId;
-      payload.list = plist;
-      payload.size = psize;
-
-      int threadReturnValue;
-      SDL_WaitThread(me.mThreadID, &threadReturnValue);
-      me.mThreadID = SDL_CreateThread(thread_function, p);
-   };
-   virtual ~cBubbleCollisionReportThread() { int threadReturnValue; SDL_WaitThread(mThreadID, &threadReturnValue); };
-};
-
 
 class cBubbleEngine : public cTimerWrapper
 {
